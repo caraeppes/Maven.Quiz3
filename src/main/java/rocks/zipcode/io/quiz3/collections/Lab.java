45386eb3bc7d2b4d7ba7a32c516d3b28005e46f9
Cara Eppes <ceppes@udel.edu>
2019-03-11 11:14:34 -0400

    Completed collections section

--- src/main/java/rocks/zipcode/io/quiz3/collections/Lab.java
+++ src/main/java/rocks/zipcode/io/quiz3/collections/Lab.java
@@ -1,17 +1,31 @@
 package rocks.zipcode.io.quiz3.collections;
 
+
 /**
  * @author leon on 10/12/2018.
  */
-public class Lab {
+public class Lab implements Comparable{
+
+    private String labName;
+
     public Lab() {
         this(null);
     }
 
     public Lab(String labName) {
+        this.labName = labName;
     }
 
     public String getName() {
-        return null;
+        return labName;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return labName.compareTo(o.toString());
+    }
+
+    public String toString(){
+        return labName;
     }
 }
--- src/main/java/rocks/zipcode/io/quiz3/collections/Student.java
+++ src/main/java/rocks/zipcode/io/quiz3/collections/Student.java
@@ -3,30 +3,53 @@ package rocks.zipcode.io.quiz3.collections;
 import rocks.zipcode.io.quiz3.objectorientation.enums.LabStatus;
 
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * @author leon on 10/12/2018.
  */
 public class Student {
+
+    Map<Lab, LabStatus> map;
+
     public Student() {
-        this(null);
+        this(new TreeMap<>());
     }
 
     public Student(Map<Lab, LabStatus> map) {
+        this.map = map;
     }
 
     public Lab getLab(String labName) {
+        for(Lab lab : map.keySet()){
+            if(lab.getName().equals(labName)){
+                return lab;
+            }
+        }
         return null;
     }
 
-    public void setLabStatus(String labName, LabStatus labStatus) {
+    public void setLabStatus(String labName, LabStatus labStatus) throws UnsupportedOperationException {
+        if(getLab(labName) == null){
+            throw new UnsupportedOperationException("Must fork lab before setting lab status!");
+        }
+        map.put(getLab(labName), labStatus);
     }
 
 
     public void forkLab(Lab lab) {
+        map.put(lab, LabStatus.PENDING);
     }
 
     public LabStatus getLabStatus(String labName) {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        return map.get(getLab(labName));
+    }
+
+    public String toString(){
+        String studentString = "";
+        for(Map.Entry entry : map.entrySet()){
+            studentString += entry.getKey().toString() + " > " + entry.getValue() + "\n";
+        }
+        return studentString.substring(0, studentString.length() - 1);
     }
 }
